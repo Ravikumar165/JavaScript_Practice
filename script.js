b8e9514df14a764f1bdb9357c72e20fdf874c65e
Ravi Yadav <yadavravi803211@gmail.com>
2026-02-09 14:33:12 +0530

    js part7

--- script.js
+++ script.js
@@ -1,100 +1,43 @@
-// Function in js 
-function sayHello() {
-    console.log("Hello, World!");
+// java script part 7
+
+
+// arrow function 
+ const sum = (a,b) => {
+    console.log(a+b);
+ }
+
+
+ // implicit return 
+ const mul =(a,b)=>{
+    a*b
+ };
+
+
+ // set interval
+  let id = setInterval (() => {
+    console.log("ravi kumar");
+  }, 2000);
+  console.log(id);
+
+  let id2 = setInterval (() => {
+    console.log("hello world");
+  }, 4000);
+  console.log(id2);// ye run hota rahega // stop karne ke liye clearInterval(id) karenge
+
+//this with arrow function 
+
+const student = {
+    name: "Ravi kumar",
+    age:21,
+    marks:86,
+    prop: this // global scope
+    getName:function(){
+        return this.name;
+    },
+  
 };
-// Call the functionsayHello();
-sayHello();
-
-
-// Function  call printname
-function printname() {
-    console.log("My name is John Doe.");
-}
-printname();
 
 
 
-// Function with parameters
-function greet(name) {
-    console.log("Hello, " + name + "!");
-}
 
-greet("Alice");
-
-// Function with return value
-
-function add(a, b) {
-    return a + b;
-}
-var result = add(5, 3);
-console.log("The sum is: " + result);
-
-// function loop
-function countToFive() {
-    for (var i = 1; i <= 5; i++) {
-        console.log(i);
-    }
-}
-countToFive();
-
-// Function with default parameters
-
-function greet(name = "Guest") {
-    console.log("Hello, " + name + "!");
-}
-greet(); // Output: Hello, Guest!
-greet("Alice"); // Output: Hello, Alice!
-
-// Function expression
-var sayGoodbye = function() {
-    console.log("Goodbye, World!");
-};
-sayGoodbye();
-
-// function adult 
-function Adult(age) {
-    
-    if (age >= 18) {
-        console.log("You are an adult.");
-    } else {
-        console.log("You are not an adult.");
-    }
-}
-
-Adult(20); 
-
-
-// taking input from user check adult or not
-function checkadult() {
-    var age = prompt("Please enter your age:");
-    if age >= 18 {
-        console.log("You are an adult.");
-    } else {
-        console.log("You are not an adult.");
-    }
-}
-checkadult();
-
-// print poem
-function printpoem() {
-    console.log("twinkle, twinkle, little star,");
-    console.log("How I wonder what you are!");
-    console.log("Up above the world so high,");
-    console.log("Like a diamond in the sky.");
-}   
-printpoem();
-
-// PQ create  a function  to roll a dice & always display the value of the dice between 1 to 6
-function rollDice() {
-    let diceValue = Math.floor(Math.random() * 6) + 1;
-    console.log("You rolled a " + diceValue);
-}
-rollDice();
-
-// function with arguments
-function calculateArea(radius) {
-    let area = Math.PI * radius * radius;
-    console.log("The area of the circle is: " + area.toFixed(2));
-}
-calculateArea(5);
 
